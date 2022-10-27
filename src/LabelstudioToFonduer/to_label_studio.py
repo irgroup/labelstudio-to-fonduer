51fc0b8ce2bb9e3422c9a41f73cc9c12fb5b1a91
jueri <jueri.keller@smail.th-koeln.de>
2022-10-27 16:54:02 +0200

    add docstrings

--- src/LabelstudioToFonduer/to_label_studio.py
+++ src/LabelstudioToFonduer/to_label_studio.py
@@ -2,8 +2,8 @@
 """Convert Fonduer candidates to Label Studio annotations."""
 
 import json
-import sqlalchemy
-from typing import List, Any
+from typing import Any, Dict, List, Union
+
 import lxml.etree
 
 
@@ -13,7 +13,6 @@ def green(text, from_, to):
 
 
 def log_offset(span, html_document, offset_start, offset_end, offset_plus, xpath_start):
-
     dom = lxml.etree.ElementTree(lxml.html.fromstring(html_document))
     results = dom.xpath(xpath_start)
 
@@ -35,15 +34,43 @@ def log_offset(span, html_document, offset_start, offset_end, offset_plus, xpath
 
 
 class FonduerToLabelStudio:
-    def __init__(self, lokal_files) -> None:
-        self.lokal_files = lokal_files
+    """Transfere Fonduer candidates to Label Studio Labes."""
 
-    def make_type(self, FD_type: str) -> str:
-        return FD_type[0].upper() + FD_type[1:]
+    def seriealize_relation(self, relation: Any) -> List[Dict[str, Any]]:
+        """Serialize a Fonduer candidate relation into a Label Studio annotated relation of entities.
 
-    def seriealize_relation(self, relation):
-        def calculate_offset_plus(FD_span, html_string, xpath, offset_start):
-            dom = lxml.etree.ElementTree(lxml.html.fromstring(html_string))
+        The Fonduer candidate is parsed and modified to create a Label Studio annotation from it.
+        First the Relation is created and then all enteties are added. The offset is modified to
+        match the HTML element span, the XPath is modifies to use the body element as root.
+
+        Args:
+            relation (Any): Fonduer candidate relation.
+
+        Returns:
+            List[Dict[str, Any]]: List of serialized relations and entities.
+        """
+
+        def calculate_offset_plus(
+            FD_span: str, html_string: str, xpath: str, offset_start: int
+        ) -> int:
+            """Calculate addititional offset from the html element.
+
+            Fondue calculates the offset based on the sentence only. If the HTML element located by
+            the XPath may contain further sentences and the HTML tag may also contain training
+             whitespaces, an additional offset may be needed.
+            This offset is calculated by searching the Fonduer Span in the HTML element and
+            subtracting the offset from fonuer.
+
+            Args:
+                FD_span (str): Text of the entity from Fonduer.
+                html_string (str): Full HTML string to construct the DOM and search the XPath.
+                xpath (str): XPath to the HTML element with the span init.
+                offset_start (int): Fonduer offset.
+
+            Returns:
+                int: Additional character offset.
+            """
+            dom = lxml.etree.ElementTree(lxml.html.fromstring(html_string))  # Create the dom
             results = dom.xpath(xpath)
 
             if len(results) > 1:
@@ -52,6 +79,7 @@ class FonduerToLabelStudio:
             if results:
                 HTML_span = results[0].text_content()
 
+                # TODO: What if the span is multiple times in the context.
                 offset_plus = HTML_span.find(FD_span)  # Search for the FD span in the LS span
 
                 if offset_plus < 1:
@@ -65,9 +93,11 @@ class FonduerToLabelStudio:
 
         html_document = relation.document.text
 
+        # TODO: Make confidence score accessible from the outside
         score = 0.90
 
         results = []
+
         # add relation
         results.append(
             {
@@ -75,10 +105,11 @@ class FonduerToLabelStudio:
                 "to_id": relation[1].id,
                 "type": "relation",
                 "direction": "right",
+                "readonly": False,
             }
         )
 
-        # add entetys
+        # add enteties
         for entity in relation:
             span_mention = entity[0]
 
@@ -97,10 +128,9 @@ class FonduerToLabelStudio:
             offset_start = span_mention.char_start + offset_plus
             offset_end = span_mention.char_end + offset_plus + 1
 
+            # TODO: improve logging
             if True:
                 log_offset(text, html_document, offset_start, offset_end, offset_plus, xpath_start)
-
-            score = 0.80
 
             result = {
                 "id": entity.id,
@@ -116,34 +146,49 @@ class FonduerToLabelStudio:
                     "startOffset": offset_start,
                     "endOffset": offset_end,
                     "text": text,
-                    "hypertextlabels": [self.make_type(entity.type)],
+                    "hypertextlabels": [entity.type[0].upper() + entity.type[1:]],
                 },
             }
             results.append(result)
 
         return results
 
-    def create_import(self, candidates: List[Any], fonduer_export_path: str) -> str:
+    def create_export(
+        self, candidates: List[Any], fonduer_export_path: str = ""
+    ) -> Union[str, List[Any]]:
+        """Create a Label Studio import file from Fonduer candidates.
 
+        Args:
+            candidates (List[Any]): Candidates from Fonduer.
+            fonduer_export_path (str, optional): Desired location for the Label Studio import file. Defaults to "".
+
+        Returns:
+            Union[str, List[Any]]: Import dictionary or the
+            path where the import file is saved.
+        """
         documents = {}
-        # serialize all candidates
-        for relation in candidates[0]:
-            if relation.document.name not in documents:
+
+        for relation in candidates[0]:  # serialize all candidates
+            if relation.document.name not in documents:  # Goup by documents
+                # Create base document
                 documents[relation.document.name] = {
                     "id": relation.document.name,
                     "data": {
                         "text": relation.document.text,
                     },
-                    "predictions": [{"model_version": 0, "score": 0, "result": []}],
+                    "annotations": [{"model_version": 0, "score": 0, "result": []}],
                 }
 
-            documents[relation.document.name]["predictions"][0]["result"].extend(
+            # Add relations of document
+            documents[relation.document.name]["annotations"][0]["result"].extend(
                 self.seriealize_relation(relation)
             )
 
-            label_studio_import = list(documents.values())
+        fonduer_export = list(documents.values())
 
-        # write tasks
-        with open(fonduer_export_path, "w") as file:
-            json.dump(label_studio_import, file)
-        return fonduer_export_path
+        if fonduer_export_path:
+            with open(fonduer_export_path, "w") as file:
+                json.dump(fonduer_export, file)
+            return fonduer_export_path
+        else:
+            return fonduer_export
