e4d30519b55cfdd96fa2b8f3024e8212b3aceaaa
jueri <jueri.keller@smail.th-koeln.de>
2022-11-03 09:09:21 +0100

    improve reference and licence for original code.

--- src/LabelstudioToFonduer/parser.py
+++ src/LabelstudioToFonduer/parser.py
@@ -1,6 +1,39 @@
-"""Modified Fonduer Spacy parser. Please see: https://github.com/HazyResearch/fonduer/blob/master/src/fonduer/parser/lingual_parser/spacy_parser.py for the original work.
-TODO: improve citation
-The `split_sentences` function is modified to split sentences only on `.` chars but obey exceptions.
+"""
+*    Title: Fonduer: Knowledge Base Construction from Richly Formatted Data
+*    Author: Wu, Sen and Hsiao, Luke and Cheng, Xiao and Hancock, Braden and Rekatsinas, Theodoros 
+            and Levis, Philip and Ré, Christopher,
+*    Date: 3.11.2022
+*    Code version: v0.9.0
+*    Availability: https://github.com/HazyResearch/fonduer/blob/master/src/fonduer/parser/lingual_parser/spacy_parser.py
+
+Original License:
+
+MIT License
+
+Copyright (c) 2018 HazyResearch
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+Modifications:
+The Fonduer Spacy parser is modified so that the `split_sentences` function split sentences only 
+on `.` chars but obey given exceptions.
 """
 import re
 import importlib
